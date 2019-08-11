6431cb13c0a3743adbbd204bdcf049c4f70131b6
admin <13265483966@163.com>
2019-08-11 23:00:52 +0800

    webpack hello world

---
+++ dayly7/firstWebpackDemo/src/add-content.js
@@ -0,0 +1,3 @@
+export default function(){
+    document.write('i\m using npm scripts!')
+}
---
+++ dayly7/firstWebpackDemo/src/index.js
@@ -0,0 +1,3 @@
+import addContent from './add-content'
+document.write('My first webpack app <br/>')
+addContent()
